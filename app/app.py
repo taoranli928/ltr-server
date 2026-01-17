2feafa039552747ca2fe0cdb03c2d721f13bc40c
eeevyi <975678049@qq.com>
2026-01-17 10:24:42 +0800

    init project

---
+++ app/app.py
@@ -0,0 +1,17 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello from ltr-server!"
+
+
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
